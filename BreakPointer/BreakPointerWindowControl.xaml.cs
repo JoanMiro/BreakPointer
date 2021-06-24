93f9ce94b0261d8581cdce3f3a403b9fb6573f2c
Gordon Mackie <gordon.mackie@openfeature.co.uk>
2021-06-24 17:26:24 +0100

    Initial add

---
+++ BreakPointer/BreakPointerWindowControl.xaml.cs
@@ -0,0 +1,83 @@
+﻿namespace BreakPointer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using System.Diagnostics;
+    using System.Runtime.InteropServices;
+    using System.Windows.Controls;
+    using EnvDTE;
+    using Microsoft.VisualStudio.OLE.Interop;
+    using Microsoft.VisualStudio.Shell;
+    using Debugger = EnvDTE.Debugger;
+
+    /// <summary>
+    /// Interaction logic for BreakPointerWindowControl.
+    /// </summary>
+    public partial class BreakPointerWindowControl : UserControl
+    {
+        [Import] internal SVsServiceProvider ServiceProvider;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BreakPointerWindowControl"/> class.
+        /// </summary>
+        public BreakPointerWindowControl()
+        {
+            this.InitializeComponent();
+            ThreadHelper.ThrowIfNotOnUIThread();
+            // var debuggerOld = this.GetInstances().First().Debugger;
+            var debugger = this.GetDebugger();
+            this.BreakpointsListView.ItemsSource = debugger.Breakpoints;
+            Debug.WriteLine(this.BreakpointsListView.ItemsSource);
+        }
+
+        private Debugger GetDebugger()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var dte = (DTE)Marshal.GetActiveObject("VisualStudio.DTE.16.0");
+            //var dte2 = (DTE)this.ServiceProvider.GetService(typeof(DTE));
+            if (dte == null)
+            {
+                throw new ArgumentNullException(nameof(dte));
+            }
+
+            return dte.Debugger;
+        }
+
+        private IEnumerable<DTE> GetInstances()
+        {
+            IRunningObjectTable rot;
+            IEnumMoniker enumMoniker;
+            var retVal = GetRunningObjectTable(0, out rot);
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            if (retVal == 0)
+            {
+                rot.EnumRunning(out enumMoniker);
+
+                var moniker = new IMoniker[1];
+                while (enumMoniker.Next(1, moniker, out _) == 0)
+                {
+                    IBindCtx bindCtx;
+                    CreateBindCtx(0, out bindCtx);
+                    string displayName;
+                    moniker[0].GetDisplayName(bindCtx, null, out displayName);
+                    Console.WriteLine("Display Name: {0}", displayName);
+                    var isVisualStudio = displayName.StartsWith("!VisualStudio");
+                    if (isVisualStudio)
+
+                    {
+                        rot.GetObject(moniker[0], out var dte);
+                        yield return dte as DTE;
+                    }
+                }
+            }
+        }
+
+        [DllImport("ole32.dll")]
+        private static extern void CreateBindCtx(int reserved, out IBindCtx ppbc);
+
+        [DllImport("ole32.dll")]
+        private static extern int GetRunningObjectTable(int reserved, out IRunningObjectTable prot);
+    }
+}
